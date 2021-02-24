0a8482314db6b1bc08441290697aeccb4b00a6c4
xiaopifu <2899018469@qq.com>
2021-02-24 09:18:59 +0800

    完成了主页功能

--- assets/js/baseapi.js
+++ assets/js/baseapi.js
@@ -1,3 +1,20 @@
-$.ajaxPrefilter(function(option) {
-    option.url = 'http://ajax.frontend.itheima.net'+option.url
-}) 
+$.ajaxPrefilter(function (option) {
+    option.url = 'http://ajax.frontend.itheima.net' + option.url
+    // 统一为有权限的接口，设置 headers 请求头
+    if (option.url.indexOf('/my/') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+        option.complete = function (res) {
+            // console.log('执行了 complete 回调：')
+            // console.log(res)
+            // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 1. 强制清空 token
+                localStorage.removeItem('token')
+                // 2. 强制跳转到登录页面
+                location.href = '/login.html'
+            }
+
+        }}
+    }) 
